2edd9e9038946831e5634375d0590abc919909b9
Md shamsuddin Hasan <122981083+Shams797@users.noreply.github.com>
2026-05-15 02:17:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ const port=3000;
 mongoose.connect(DB_PATH).then(()=>{
   console.log("connected to database mongoose successfully");
   app.listen(port,()=>{
-    console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on port ${port}`);
   })
 }).catch(err=>{
     console.log("error in connecting to database",err);
